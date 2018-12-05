70152e1e025027c602ae5ac86c874a84f564c96c
豪少 <1051973995@qq.com>
2018-12-06 00:01:17 +0800

    配置文件

---
+++ precache-manifest.413281c4cb53c626ad9ab5efa062650d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "153d4bc7d4145f3061db",
+    "url": "./static/js/runtime~main.153d4bc7.js"
+  },
+  {
+    "revision": "47cca1229fcef0cfc2a1",
+    "url": "./static/js/main.47cca122.chunk.js"
+  },
+  {
+    "revision": "217354c7c9664c26bc7d",
+    "url": "./static/js/2.217354c7.chunk.js"
+  },
+  {
+    "revision": "b6897e9c455a2f0c378d",
+    "url": "./static/js/1.b6897e9c.chunk.js"
+  },
+  {
+    "revision": "217354c7c9664c26bc7d",
+    "url": "./static/css/2.c89d3819.chunk.css"
+  },
+  {
+    "revision": "866c5969e239211e2531670914fc5c95",
+    "url": "./index.html"
+  }
+];
